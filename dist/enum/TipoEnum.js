106598127a8a7e77811014f774c74c3bc3006f9c
Mats23 <mateus-vinicius19@live.com>
2019-03-10 17:10:56 -0300

    remover

--- dist/enum/TipoEnum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TipoEnum;
-(function (TipoEnum) {
-    TipoEnum["especifico"] = "especifico";
-    TipoEnum["diariamente"] = "diariamente";
-    TipoEnum["semanalmente"] = "semanalmente";
-})(TipoEnum = exports.TipoEnum || (exports.TipoEnum = {}));
